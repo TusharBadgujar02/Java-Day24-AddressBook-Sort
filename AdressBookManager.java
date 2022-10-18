7b9f2fb9a7345aac60239ab24adc2f64c57b5352
9685959802 <tusharbadgujar0000@gmail.com>
2022-10-18 08:55:09 +0530

    Ability to sort the entries in addressbook alphabatically by person's name

---
+++ AdressBookManager.java
@@ -0,0 +1,10 @@
+package com.addressbooksort;
+
+public class AdressBookManager {
+	public static void main(String[] args) {
+		AddressBook entry = new AddressBook();
+	
+		System.out.println("Welcome to Address Book Program in Java!");
+		entry.manageAddressBookList();
+	}
+}
